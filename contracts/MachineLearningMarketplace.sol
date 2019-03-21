c6e3d5aedfdce419252253bc2bc9c859d85c13b2
merlox <merunasgrincalaitis@gmail.com>
2019-03-21 15:30:48 +0100

    Added choosing function

--- contracts/MachineLearningMarketplace.sol
+++ contracts/MachineLearningMarketplace.sol
@@ -1,12 +1,13 @@
-// We need a way for buyers and sellers to interact, a function to place an order for training a model and a function to submit training by sellers after they've trained the model with their results. The sellers will upload the dataset json url they got and the resulting weight and bias. The buyers will upload 1 or more datasets with an array of URLs which have to be json files that can be uploaded for free on github, from 1 to 10 elements of data to verify the cost of the trained model and the payment for training. Each model will have a maximum time of 3 days to be trained.
+// We need a way for buyers and sellers to interact, a function to place an order for training a model and a function to submit training by sellers after they've trained the model with their results. The sellers will upload the dataset json url they got and the resulting weight and bias. The buyers will upload 1 or more datasets with an array of URLs which have to be json files that can be uploaded for free on github, from 1 to 10 elements of data to verify the cost of the trained model and the payment for training. Each model will have a maximum time of 3 days to be trained which means that the buyer has to choose a winner before 3 full days.
 // We also need functions to verify the quality of the trained model which will be done with a simple cost function that will return the error for a particular data field.
-
 pragma solidity 0.5.5;
 
 contract MachineLearningMarketplace {
     event AddedJob(uint256 indexed id, uint256 indexed timestamp);
+    event AddedResult(uint256 indexed id, uint256 indexed timestamp, uint256 indexed sender);
 
     struct Model {
+        uint256 id;
         string datasetUrl;
         uint256 result;
         uint256 x;
@@ -14,9 +15,10 @@ contract MachineLearningMarketplace {
         uint256 bias;
         uint256 payment;
         uint256 timestamp;
+        address owner;
     }
     mapping(uint256 => Model) public models;
-    mapping(uint256 => mapping(address => Model)) public trainedModels;
+    mapping(uint256 => Model[]) public trainedModels;
     uint256 public latestId;
 
     /// @notice To upload a model in order to train it
@@ -25,10 +27,10 @@ contract MachineLearningMarketplace {
     /// @param _x An independent variable
     function uploadJob(string memory _dataSetUrl, uint256 _result, uint256 _x) public payable {
         require(msg.value > 0, 'You must send some ether to get your model trained');
-
-        Model memory m = Model(_dataSetUrl, _result, _x, 0, 0, msg.value, now);
+        Model memory m = Model(latestId, _dataSetUrl, _result, _x, 0, 0, msg.value, now, msg.sender);
         models[latestId] = m;
         emit AddedJob(latestId, now);
+        latestId += 1;
     }
 
     /// @notice To upload the result of a trained model
@@ -36,8 +38,30 @@ contract MachineLearningMarketplace {
     /// @param _weight The final trained weight
     /// @param _bias The final trained bias
     function uploadResult(uint256 _id, uint256 _weight, uint256 _bias) public {
-        Model memory m = Model(models[_id].datasetUrl, models[_id].result, models[_id].x, _weight, _bias, models[_id].payment, now);
-        trainedModels[_id][msg.sender] = m;
+        Model memory m = Model(_id, models[_id].datasetUrl, models[_id].result, models[_id].x, _weight, _bias, models[_id].payment, now, msg.sender);
+        trainedModels[_id].push(m);
+        emit AddedResult(_id, now, msg.sender);
+    }
+
+    /// @notice To choose a winner by the sender
+    /// @param _id The id of the model
+    /// @param _arrayIdSelected The array index of the selected winner
+    function chooseResult(uint256 _id, uint256 _arrayIdSelected) public {
+        Model memory m = models[_id];
+        Model memory t = trainedModels[_id];
+        // If 3 days have passed the winner will be the first one, otherwise the owner is allowed to choose a winner before 3 full days
+        if(now - m.timestamp < 3 days) {
+            require(msg.sender == m.owner, 'Only the owner can select the winner');
+            t[_arrayIdSelected].owner.transfer(m.payment);
+        } else {
+            // If there's more than one result, send it to the first
+            if(t > 0) {
+                t[0].owner.transfer(m.payment);
+            } else {
+                // Send it to the owner if none applied to the job
+                m.owner.transfer(m.payment);
+            }
+        }
     }
 
     /// @notice The cost function implemented in solidity
